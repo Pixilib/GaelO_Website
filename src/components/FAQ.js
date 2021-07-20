e7ef0712a72183e6a245a9aa1e722090fe4c734d
unknown <displaytuto@gmail.com>
2021-07-20 09:43:22 +0200

    update faq.js

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -18,7 +18,7 @@ function About(props) {
         <Row id="about" className="bg-light p-5 text-center align-items-center border-top" style={getStyleAbout()}>
             <h3>{t('faq.title')}</h3>
             <Col className="p-5">
-                <Row className="text-start">
+                <Row className="text-start align-items-center">
                     <Col md={4}>
                         <Card border="dark">
                             <Card.Header>{t('faq.question.0')}</Card.Header>
@@ -51,7 +51,7 @@ function About(props) {
                     </Col>
                 </Row>
                 <br/>
-                <Row className="text-start">
+                <Row className="text-start align-items-center">
                     <Col md={4} >
                         <Card border="dark">
                             <Card.Header>{t('faq.question.3')}</Card.Header>
@@ -68,24 +68,14 @@ function About(props) {
                             <Card.Header>{t('faq.question.4')}</Card.Header>
                             <Card.Body>
                                 <Card.Text>
-                                    <span className="ps-4"></span>{t('faq.answer.4')}
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={4}>
-                        <Card border="dark">
-                            <Card.Header>{t('faq.question.5')}</Card.Header>
-                            <Card.Body>
-                                <Card.Text>
                                     <ul>
-                                        <li><span className="ps-4"></span>{t('faq.answer.5.0')}</li>
-                                        <li>- {t('faq.answer.5.1')}</li>
-                                        <li>- {t('faq.answer.5.2')}</li>
-                                        <li>- {t('faq.answer.5.3')}</li>
-                                        <li>- {t('faq.answer.5.4')}</li>
-                                        <li>- {t('faq.answer.5.5')}</li>
-                                        <li>- {t('faq.answer.5.6')}</li>
+                                        <li><span className="ps-4"></span>{t('faq.answer.4.0')}</li>
+                                        <li>- {t('faq.answer.4.1')}</li>
+                                        <li>- {t('faq.answer.4.2')}</li>
+                                        <li>- {t('faq.answer.4.3')}</li>
+                                        <li>- {t('faq.answer.4.4')}</li>
+                                        <li>- {t('faq.answer.4.5')}</li>
+                                        <li>- {t('faq.answer.4.6')}</li>
                                     </ul>
                                 </Card.Text>
                             </Card.Body>
