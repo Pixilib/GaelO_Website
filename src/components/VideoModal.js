68e7c12b41bf43dadb2c62c7583007c91e7a688d
Alan <a.aupetit31@gmail.com>
2021-12-06 11:33:49 +0100

    update

---
+++ src/components/VideoModal.js
@@ -0,0 +1,24 @@
+
+import {  Modal,  } from "../../node_modules/react-bootstrap";
+import { useTranslation } from "react-i18next"
+import ReactPlayer from "react-player";
+
+
+
+function VideoModal(props) {
+    const { t } = useTranslation();
+    return (
+        <Modal
+            {...props}
+            size="xl"
+            fullscreen="md-down"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Body>
+                <ReactPlayer height="800px" url={t('header.linkVideo')} width="100%" />
+            </Modal.Body>
+
+        </Modal>
+    );
+}export default VideoModal
