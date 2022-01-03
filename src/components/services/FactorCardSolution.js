859bf4244a7dbbbe72ce526e7359757b83a74aef
Maxence <maxence.delecroix@gmail.com>
2022-01-03 12:01:02 +0100

    correction bloc solution

---
+++ src/components/services/FactorCardSolution.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+
+const FactorCardSolution = (props) => {
+
+
+
+return(
+
+    <Card>{props.text}</Card>
+)
+
+
+
+}
+
+export default FactorCardSolution
