f372b82f00b1e3e0249ad47cef73e2d93b573bcf
Maxence <maxence.delecroix@gmail.com>
2022-01-03 12:01:16 +0100

    cogerence du code

--- src/components/ourviews/OurViewRoot.js
+++ src/components/ourviews/OurViewRoot.js
@@ -1,24 +1,16 @@
 
 import { Fragment } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container} from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 
 import KeyFactorSuccess from './KeyFactorSuccess'
+import OurViewsText from './OurViewsText'
 
 const OurViewRoot = () => {
     const { t } = useTranslation()
     return (
         <Fragment>
-            <Container id='ourviews' className='styleOurViews'>
-                <h1 className="text-center  ">{t('ourviews.title')}</h1>
-                <Row className=' d-flex justify-content-center'>
-                    <Col className='coltext'>
-                        <p >{t('ourviews.body1')}</p>
-                        <p >{t('ourviews.body2')}</p>
-                    </Col>
-                    <Col />
-                </Row>
-            </Container>
+            <OurViewsText/>
             <Container>
                     <KeyFactorSuccess />
             </Container>
---
+++ src/components/ourviews/OurViewsText.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Col, Row, Container } from 'react-bootstrap'
+
+const OurViewsText = () => {
+
+const {t} = useTranslation()
+
+return(
+
+    <Container id='ourviews' className='styleOurViews'>
+    <h1 className="text-center  ">{t('ourviews.title')}</h1>
+    <Row className=' d-flex justify-content-center'>
+        <Col className='coltext'>
+            <p >{t('ourviews.body1')}</p>
+            <p >{t('ourviews.body2')}</p>
+        </Col>
+        <Col />
+    </Row>
+</Container>
+)
+
+
+}
+
+export default OurViewsText
