b2b76db440859608ba6a4a8f6e61743ed5ebd1c2
Maxence <maxence.delecroix@gmail.com>
2022-01-04 11:38:03 +0100

    partner dynamique

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,24 +1,32 @@
 import React from "react";
-import { Row , Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import { useTranslation } from "react-i18next";
+import fr from '../lang/fr.json'
+
+const Partner = () => {
+
+    const { t } = useTranslation()
+
+    const generatePartner = () => {
+        const partnerNumero = [...Array(fr.partner.logo.length).keys()]
+
+        const componentArray = partnerNumero.map((item) => {
+            return (
+                <Col>
+                    <img src={t('partner.logo.' + item)}></img>
+                </Col>
+            )
+        })
+        return componentArray
+    }
 
 
-const Partner =()=>{
-
-    const{t}=useTranslation()
-return(
-   <Row>
-       <h1>{t('partner.title')}</h1>
-        <Col><img src="Lysa.svg" className="logocontact" ></img></Col>
-        <Col><img src="Lysarc.svg" className="logocontact"></img></Col>
-        <Col><img src="carnot.svg" className="logocontact"></img></Col>
-        <Col><img src="ovh.svg " className="logocontact"></img></Col>
-   </Row>
-)
-
-
-
-
+    return (
+        <Row>
+            <h1>{t('partner.title')}</h1>
+            {generatePartner()}
+        </Row>
+    )
 }
 
 export default Partner
