3cec086e5ead65448a740a793701db99eb4f0ef0
Maxence <maxence.delecroix@gmail.com>
2022-01-04 17:29:31 +0100

    debut stylisation ServiceCard

--- src/components/services/ServiceCard.js
+++ src/components/services/ServiceCard.js
@@ -1,20 +1,30 @@
 import { Container, Col, Row } from "react-bootstrap";
-
+import { Card } from "react-bootstrap";
+import CardHeader from "react-bootstrap/esm/CardHeader";
 import { useTranslation } from "react-i18next";
 import fr from '../../lang/fr.json'
-import FactorCardSolution from "./FactorCardSolution";
+import CardFade from "./FactorCardService";
 
 const ServiceCard = () => {
 
     const { t } = useTranslation()
 
     const generateCardSolution = () => {
-        const solutionNumber = [...Array(fr.service.card.length).keys()]
+        const solutionNumber = [...Array(fr.service.subtitle.length).keys()]
 
         const componentArray = solutionNumber.map((item) => {
             return (
                 <Col  >
-                    <FactorCardSolution text={t('service.card.' + item)} />
+                    <CardFade>
+                       
+                        {/* vue du front de la card */}
+                        <Card className="text-center fs-4 text-primary cardService">{t('service.subtitle.'+item)}
+                         <img className="serviceIcon " src={t('service.logo.'+item)}></img>  {t('service.front.'+item)}</Card>
+                        {/* vue du back de la card */}
+                        <Card className="text-center">{t('service.back.'+item)}</Card>
+                        <button>Read More</button>
+                    </CardFade>
+
                 </Col>
             )
         })
@@ -24,7 +34,7 @@ const ServiceCard = () => {
     return (
 
         <Container>
-            <Row className='row-cols-md-3 '>
+            <Row className='w-100 h-100 '>
                 {generateCardSolution()}
             </Row>
         </Container>
