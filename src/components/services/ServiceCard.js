3bd0377d323a5c1cff74efdcd3f0c62d76501122
Maxence <maxence.delecroix@gmail.com>
2022-01-05 12:16:20 +0100

    test fade

--- src/components/services/ServiceCard.js
+++ src/components/services/ServiceCard.js
@@ -2,9 +2,16 @@ import { Container, Col, Row } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import fr from '../../lang/fr.json'
+import { CSSTransition } from "react-transition-group";
 import CardFade from "./CardFade";
+import { useState } from "react";
 
 const ServiceCard = () => {
+
+    const [isFront, setIsFront] = useState(true);
+
+    console.log(isFront, "je suis rentré")
+
 
     const { t } = useTranslation()
 
@@ -13,20 +20,25 @@ const ServiceCard = () => {
 
         const componentArray = solutionNumber.map((item) => {
             return (
-                <Col  >
+                <Col>
                     <CardFade>
+                        <CSSTransition in={isFront} timeout={200}
+                        onEnter={()=>setIsFront(false)}
+                        onExit={()=>setIsFront(true)}
+                         classNames="fade">
+                            {/* vue du front de la card */}
+                            <Card className="cardServiceFront" onMouseEnter={() => setIsFront(true)}>
+                                <p className="text-center fw-bold fs-4 ">{t('service.subtitle.' + item)}</p>
+                                <img className="serviceIcon " src={t('service.logo.' + item)}></img>
+                                <p className="text-center text-dark">{t('service.front.' + item)}</p>
+                            </Card>
 
-                        {/* vue du front de la card */}
-                        <Card className="cardService">
-                            <p className="text-center fw-bold fs-4 ">{t('service.subtitle.' + item)}</p>
-                            <img className="serviceIcon " src={t('service.logo.' + item)}></img>
-                            <p className="text-center text-dark">{t('service.front.' + item)}</p>
-                        </Card>
-                        {/* vue du back de la card */}
-                        <Card className="cardService">
-                            <p className="text-center text-dark ">{t('service.back.' + item)}</p>
-                        </Card>
 
+                            {/* vue du back de la card */}
+                            <Card className="cardServiceBack " onMouseLeave={() => setIsFront(false)}>
+                                <p className="text-center text-dark ">{t('service.back.' + item)}</p>
+                            </Card>
+                        </CSSTransition>
                     </CardFade>
 
                 </Col>
@@ -37,11 +49,11 @@ const ServiceCard = () => {
 
     return (
 
-       
-            <Row className='w-100 h-100 row-cols-md-3 d-flex rowCard'>
-                {generateCardSolution()}
-            </Row>
-        
+
+        <Row className='w-100 h-100 row-cols-md-3 d-flex rowCard'>
+            {generateCardSolution()}
+        </Row>
+
     )
 
 }
