733bead2a864a474fdc3e81401ae966d82b3f4ed
Maxence <maxence.delecroix@gmail.com>
2022-01-05 13:30:54 +0100

    Rendu Fade mise en place , manque les details et Color pour chaque card

--- src/components/services/CardFade.js
+++ src/components/services/CardFade.js
@@ -1,19 +1,25 @@
-import React ,{Fragment} from "react";
-import { Card } from "react-bootstrap";
+import React, { Fragment,useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 
 
+const CardFade = (props) => {
 
-const CardFade =(props)=>{
+    const [isFront, setIsFront] = useState(true);
+    
     console.log(props.children)
-    return(
+
+    return (
+
         <Fragment>
-            <Card className="CardFade ">
-                {props.children[0]}
-            </Card>
-            <Card className="CardFade">
-                {props.children[1]}
-            </Card>
+            <CSSTransition in={isFront} timeout={400}
+                classNames="fade">
+                
+                    <div onMouseEnter={() => setIsFront(false)} onMouseLeave={() => setIsFront(true)}>{isFront ? props.children[0] : props.children[1]}</div>
+                
+             
+                
+            </CSSTransition>
         </Fragment>
     )
 }
--- src/components/services/ServiceCard.js
+++ src/components/services/ServiceCard.js
@@ -1,16 +1,16 @@
-import { Container, Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import fr from '../../lang/fr.json'
-import { CSSTransition } from "react-transition-group";
+
 import CardFade from "./CardFade";
-import { useState } from "react";
+
 
 const ServiceCard = () => {
 
-    const [isFront, setIsFront] = useState(true);
 
-    console.log(isFront, "je suis rentré")
+
+
 
 
     const { t } = useTranslation()
@@ -22,23 +22,17 @@ const ServiceCard = () => {
             return (
                 <Col>
                     <CardFade>
-                        <CSSTransition in={isFront} timeout={200}
-                        onEnter={()=>setIsFront(false)}
-                        onExit={()=>setIsFront(true)}
-                         classNames="fade">
-                            {/* vue du front de la card */}
-                            <Card className="cardServiceFront" onMouseEnter={() => setIsFront(true)}>
-                                <p className="text-center fw-bold fs-4 ">{t('service.subtitle.' + item)}</p>
-                                <img className="serviceIcon " src={t('service.logo.' + item)}></img>
-                                <p className="text-center text-dark">{t('service.front.' + item)}</p>
-                            </Card>
+                        {/* vue du front de la card */}
+                        <Card className="cardServiceFront">
+                            <p className="text-center fw-bold fs-4 ">{t('service.subtitle.' + item)}</p>
+                            <img className="serviceIcon " src={t('service.logo.' + item)}></img>
+                            <p className="text-center text-dark">{t('service.front.' + item)}</p>
+                        </Card>
+                        {/* vue du back de la card */}
+                        <Card className="cardServiceBack " >
+                            <p className="text-center text-dark ">{t('service.back.' + item)}</p>
+                        </Card>
 
-
-                            {/* vue du back de la card */}
-                            <Card className="cardServiceBack " onMouseLeave={() => setIsFront(false)}>
-                                <p className="text-center text-dark ">{t('service.back.' + item)}</p>
-                            </Card>
-                        </CSSTransition>
                     </CardFade>
 
                 </Col>
