4890b5784a1f4ec352b82b5ded9c2c1abd66d95b
Maxence <maxence.delecroix@gmail.com>
2022-01-11 16:02:07 +0100

    contact fini + index.html seo modif

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Row } from 'react-bootstrap'
+import { Row , Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
+import logoMail from '../../assets/images/mail.svg'
 
 
 const Contact = () => {
   const { t } = useTranslation()
 
   return (
-    <Row id='contact' className='  p-5 text-center align-items-center' >
+    <Row id='contact' className='  p-5  text-center ' >
       <h1 className='text-white'>{t('contact.title')}</h1>
-      <a className='text-white'href={'mailto:' + t('contact.mail')}>{t('contact.mail')}</a>
+      < Col className='d-flex flex-column align-items-center' >
+          
+            <img className='logoMail'  src={logoMail}/>
+            <a className='text-white fs-4' href={'mailto:' + t('contact.mail')}>  {t('contact.mail')}</a>
+          
+      </Col>
       <div className="">
         <iframe title="Subscribe" height='700' width='100%' src="https://cdn.forms-content.sg-form.com/484b213d-6ca2-11ec-9aca-86faac9e4b7c" />
       </div>
@@ -19,3 +25,4 @@ const Contact = () => {
 }
 
 export default Contact
+
