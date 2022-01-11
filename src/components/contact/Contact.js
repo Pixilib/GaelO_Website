e731fd79a973e93ace2c62019163fe99777f71f7
Alan <a.aupetit31@gmail.com>
2022-01-11 13:55:38 +0100

    update contact / delete markdownmodal

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,8 +8,8 @@ const Contact = () => {
 
   return (
     <Row id='contact' className='  p-5 text-center align-items-center' >
-      <h1>{t('contact.title')}</h1>
-      <a className='text-white' href={'mailto:' + t('contact.mail')}>{t('contact.mail')}</a>
+      <h1 className='text-white'>{t('contact.title')}</h1>
+      <a className='text-white'href={'mailto:' + t('contact.mail')}>{t('contact.mail')}</a>
       <div className="">
         <iframe title="Subscribe" height='700' width='100%' src="https://cdn.forms-content.sg-form.com/484b213d-6ca2-11ec-9aca-86faac9e4b7c" />
       </div>
