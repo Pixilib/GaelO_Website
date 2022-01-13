06563624f9d1a454815abe7581c936dd2f344177
Alan <a.aupetit31@gmail.com>
2022-01-13 09:49:36 +0100

    page 404 up versioning

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { BrowserRouter } from 'react-router-dom'
+import Error404 from './components/404/404error'
+
+import { BrowserRouter, Route,  Routes } from 'react-router-dom'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
+    <Routes>
+      <Route path='*' element={<Error404/>}/> 
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
