d4a2ff3c733f851193c2cac5508168f60d75c3aa
Salim Kanoun <salim.kanoun@gmail.com>
2022-03-18 10:47:16 +0100

    add pixilib video

--- src/components/services/ServiceRoot.js
+++ src/components/services/ServiceRoot.js
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import ReactPlayer from "react-player";
 import ServiceCard from "./ServiceCard";
 
 const ServiceRoot = () => {
@@ -10,8 +11,21 @@ const ServiceRoot = () => {
 
         <Container >
             <h1 className="text-center">{t('service.title')} </h1>
-            <Row className="d-flex justify-content-center align-items-center" >
-                <Col xl={4 } className="fw-bold">
+            <Row className="mt-5 d-flex justify-content-center align-items-center" >
+                <Col xl={4} >
+                    <Row className="d-flex justify-content-center align-items-center">
+                        <img alt={'Pixilib Logo'} style={{ 'max-width': "250px" }} src={t('pixilib.jpg')}></img>
+                    </Row>
+                    <Row className="d-flex justify-content-center align-items-center fw-bold text-info fs-5 text-center">
+                        {t('service.pixilib.intro')}
+                    </Row>
+                </Col>
+                <Col xl={8} className="d-flex justify-content-center align-items-right">
+                    <ReactPlayer controls="true" url={t('service.pixilib.video')} />
+                </Col>
+            </Row>
+            <Row className="mt-5 d-flex justify-content-center align-items-center" >
+                <Col xl={4} className="fw-bold">
                     <p>{t('service.intro')}</p>
                     <p>{t('service.content')}</p>
                 </Col>
