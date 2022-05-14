80fb8b66248e0c3fca79f61a904fe7505e59fdb7
Josselin Caer <josselin.caer@gmail.com>
2022-05-14 16:17:39 -0300

    Updated yt icon

--- src/components/header/Welcome.js
+++ src/components/header/Welcome.js
@@ -1,62 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import { Row, Figure, Container } from 'react-bootstrap'
-import { useTranslation } from 'react-i18next'
-import VideoModal from './VideoModal'
-import logoPrincipalWhite from '../../assets/images/gaelo-logo-white.svg'
-
+import React, { useEffect, useState } from "react";
+import { Row, Figure, Container } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import VideoModal from "./VideoModal";
+import logoPrincipalWhite from "../../assets/images/gaelo-logo-white.svg";
+import youtubePlay from "../../assets/images/youtube_play.svg";
 
 const Header = () => {
-
   const getStyleRow = (h, w) => {
     return {
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      height: h + 'px',
-      width: '100%',
-    }
-  }
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      height: h + "px",
+      width: "100%",
+    };
+  };
 
   const getWindowDimensions = () => {
-    const { innerWidth: width, innerHeight: height } = window
+    const { innerWidth: width, innerHeight: height } = window;
     return {
       width,
-      height
-    }
-  }
+      height,
+    };
+  };
 
   const useWindowDimensions = () => {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions()
+    );
 
     useEffect(() => {
       function handleResize() {
-        setWindowDimensions(getWindowDimensions())
+        setWindowDimensions(getWindowDimensions());
       }
 
-      window.addEventListener('resize', handleResize)
-      return () => window.removeEventListener('resize', handleResize)
-    }, [])
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-    return windowDimensions
-  }
+    return windowDimensions;
+  };
 
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
-  const [modalShow, setModalShow] = React.useState(false)
+  const [modalShow, setModalShow] = React.useState(false);
 
   return (
-    <Container style={getStyleRow(useWindowDimensions().height, useWindowDimensions().width)} className='  d-flex text-center justify-content-center flex-column align-items-center'>      
-          <Figure.Image className='w-50' src={logoPrincipalWhite} alt="GaelO" />
-          <p className='text-white fs-4 fw-bold'>{t('header.title')}</p>
-        
-    <Row className="pt-3" >
-        <button className='btn-ytb' type='button' onClick={() => setModalShow(true)}>
-          <img alt="video" src='logo-video.png' variant='primary' className='logoVideo' />
-          <p className='m-0'>{t('header.button')}</p>
+    <Container
+      style={getStyleRow(
+        useWindowDimensions().height,
+        useWindowDimensions().width
+      )}
+      className="  d-flex text-center justify-content-center flex-column align-items-center"
+    >
+      <Figure.Image className="w-50" src={logoPrincipalWhite} alt="GaelO" />
+      <p className="text-white fs-4 fw-bold">{t("header.title")}</p>
+
+      <Row className="pt-3">
+        <button
+          className="btn-ytb"
+          type="button"
+          onClick={() => setModalShow(true)}
+        >
+          <img
+            alt="video"
+            src={youtubePlay}
+            variant="primary"
+            className="logoVideo"
+          />
+          <p className="m-0">{t("header.button")}</p>
         </button>
         <VideoModal show={modalShow} onHide={() => setModalShow(false)} />
-        </Row>
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
