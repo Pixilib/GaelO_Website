cec3739b4ca07635ea6e01cc6b281ec0677d5257
Josselin Caer <josselin.caer@gmail.com>
2022-05-19 17:09:19 -0300

    Updated bootstrap import

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,7 @@ import Ourteam from "./team/OurTeam";
 import Footer from "./footer/Footer";
 import Contact from "./contact/Contact";
 
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../assets/css/gaelo.css";
 
 const defaultLanguage = Language.FR;
